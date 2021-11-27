d8f1059c078a1cb05019bfe549cdb36ba85225d8
jkmathes <jack@jkmathes.org>
2021-11-27 10:41:23 -0600

    Remove old dependencies

--- lib/files.go
+++ lib/files.go
@@ -1,27 +1,10 @@
 package lib
 
 import (
-	"embed"
 	_ "embed"
 	"io"
-	"log"
 	"os"
 )
-
-//go:embed resources/*
-var resourceFs embed.FS
-
-func LinuxAsm() string {
-	return readFile("resources/linux.s")
-}
-
-func readFile(f string) string {
-	data, err := resourceFs.ReadFile(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return string(data)
-}
 
 func CopyFile(src string, dest string) {
 	d, err := os.Create(dest)
